fad0222cb815ba9baec653dac8d52d15cb56a195
madan limbu <madanl@deeson.com>
2019-06-02 03:54:41 +0100

    Testing with UI

--- public/test.js
+++ public/test.js
@@ -1,17 +1,18 @@
 class Test extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { test: props.test, dummy: props.dummy}
+    this.state = {
+      test: props.test,
+      dummy: props.dummy,
+      host: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/get?res=`,
+      url: '',
+    }
+    this.updateData = this.updateData.bind(this)
   }
 
-  componentDidMount() {
-    setTimeout(() => this.testCORS(), 3000)
-    let local_test = `http://localhost:9000/api`
-    let url = `https://password.madanlimbu.com/dummy`
-    let proxy_api = `http://localhost:9000/get?res=${url}`
-
-    this.getData(url, data => this.setState({test: JSON.stringify(data)}))
-    this.getData(proxy_api, data => this.setState({dummy: JSON.stringify(data)}))
+  updateData(){
+    this.getData(this.state.url, data => this.setState({test: JSON.stringify(data)}))
+    this.getData(`${this.state.host}${this.state.url}`, (data) => this.setState({dummy: JSON.stringify(data)}))
   }
 
   getData(url, callback) {
@@ -23,17 +24,37 @@ class Test extends React.Component {
 
   render() {
     return (
-      <div>
+        <div>
         <h2>Testing corcs call back</h2>
-        <div className="test__normal-data">Using normal url : <code>{this.state.test}</code></div>
+          <div>
+            <label>Submit strict CORS URL endpoint to Test Proxy Api : </label>
+            <input
+              size={55}
+              placeholder={"url"}
+              type="text"
+              name="url"
+              value={this.state.url}
+              onChange={(e)=>this.setState({url: e.target.value})}/>
+            <button
+              type="submit"
+              onClick={(e) => { e.preventDefault; this.updateData(this.state.url)}}
+            >
+              Test URL
+            </button>
+          </div>
+          <br/>
+        <div className="test__normal-data">Using normal url : {this.state.url}<br/><code>{this.state.test}</code></div>
         <br/>
-        <div className="test__proxy-data">Using middle ware: <code>{this.state.dummy}</code></div>
+        <div className="test__proxy-data">Using middle ware: {(this.state.url) ? `${this.state.host}${this.state.url}` : ''}<br/><code>{this.state.dummy}</code></div>
       </div>
     )
   }
 }
 
 const root = document.querySelector('#root')
-ReactDOM.render(<Test test={`This will be updated with api data.`}/>, root)
+ReactDOM.render(<Test
+  test={`This will be updated with normal api fetch data.`}
+  dummy={`This will be updated with proxy api fetch data.`}
+/>, root)
 
 //Todo: Better tools for frontend dev, homepage creation, form for submiting custom api end point
